a74aa77ac286db3cec277e83af353d207d025cec
joaopedroraldi <joaopedroraldi@gmail.com>
2019-11-12 10:42:09 -0200

    Removido versão inicial do pawt por joão pedro

--- footer.php
+++
@@ -1,9 +0,0 @@
-<div class="footer">
-	<div class="container-fluid">
-		<div class="row">
-			<div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
-				Todos os direitos autorais reservados. Dashboard feito por <a href="https://colorlib.com/wp/">Colorlib</a> <small>(manter crédito)</small>.
-			</div>
-		</div>
-	</div>
-</div>
